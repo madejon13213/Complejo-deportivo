172b799354cd4641341698b7851a1b5ec752fd92
madej0n <javiermadejon1@gmail.com>
2026-04-21 12:34:18 +0200

    refactor: use reservation repository in service

--- backend/app/services/reservation_service.py
+++ backend/app/services/reservation_service.py
@@ -1,18 +1,19 @@
-from fastapi import HTTPException, Response, status
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-from app.tables.tables import Reserva
+
+from app.repositories.reservation_repository import ReservationRepository
 from app.schemas.reservation_schema import ReservationResponse
 
-class ReservationService:
 
+class ReservationService:
     @staticmethod
     def get_all_reservations(db: Session) -> list[ReservationResponse]:
+        repo = ReservationRepository(db)
         try:
-            reservations = db.query(Reserva).all()
+            reservations = repo.get_all()
             return reservations
-        except Exception as e:
+        except Exception:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="Error al intentar recuperar la lista de reservas de la base de datos"
+                detail="Error al intentar recuperar la lista de reservas de la base de datos",
             )
